eea5702e5d3e28a886342ec1c9cba583f492d048
unknown <work@ixiaozi.cn>
2023-11-21 20:48:10 +0800

    *

--- stun.sh
+++ stun.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+file="public/stun.json"
+token="CmH6FDe1DR7Gr5hycfiasjFlYa7yGKpp"
+
 cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
 
-file="public/stun.json"
-
-# Step 1: 获取token
-token=$(curl -s -X POST -H "Content-Type: application/json" -d '{"Account":"666","Password":"666","TwoFA":""}' http://192.168.1.11:16601/api/login | jq -r '.token')
-if [ -z "$token" ]; then
-    echo "获取token失败. Exiting."
-    exit 1
-fi
+## Step 1: 获取token
+#token=$(curl -s -X POST -H "Content-Type: application/json" -d '{"Account":"666","Password":"666","TwoFA":""}' http://192.168.1.11:16601/api/login | jq -r '.token')
+#if [ -z "$token" ]; then
+#    echo "获取token失败. Exiting."
+#    exit 1
+#fi
 
 # Step 2: 获取stun列表
-response=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: $token" "http://192.168.1.11:16601/api/stunrulelist?_=$(date +%s)" | sed 's/\\\"/'\''/g' | sed 's/"LogContent":".*","LogTime"/"LogContent":"","LogTime"/')
+response=$(curl -s -X GET -H "Content-Type: application/json" -H "OpenToken: $token" "http://192.168.1.11:16601/api/stunrulelist?_=$(date +%s)" | sed 's/\\\"/'\''/g' | sed 's/"LogContent":".*","LogTime"/"LogContent":"","LogTime"/')
 if [ -z "$response" ]; then
     echo "Error: Failed to fetch stun information."
     exit 1
