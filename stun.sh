c0859dbd4d3f263c38e6753065835142343b250c
unknown <work@ixiaozi.cn>
2023-11-21 23:07:30 +0800

    *

--- stun.sh
+++ stun.sh
@@ -5,13 +5,6 @@ token="CmH6FDe1DR7Gr5hycfiasjFlYa7yGKpp"
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
 
-## Step 1: 获取token
-#token=$(curl -s -X POST -H "Content-Type: application/json" -d '{"Account":"666","Password":"666","TwoFA":""}' http://192.168.1.11:16601/api/login | jq -r '.token')
-#if [ -z "$token" ]; then
-#    echo "获取token失败. Exiting."
-#    exit 1
-#fi
-
 # Step 2: 获取stun列表
 response=$(curl -s -X GET -H "Content-Type: application/json" -H "OpenToken: $token" "http://192.168.1.11:16601/api/stunrulelist?_=$(date +%s)" | sed 's/\\\"/'\''/g' | sed 's/"LogContent":".*","LogTime"/"LogContent":"","LogTime"/')
 if [ -z "$response" ]; then
@@ -24,10 +17,8 @@ if [ ! -s "$file" ]; then
     echo "生成文件[${file}]失败."
     exit 1
 fi
-git checkout -b origin/gh-pages
 git add .
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 git pull origin main
 git push origin main
-git checkout -b main
 echo "success!"
